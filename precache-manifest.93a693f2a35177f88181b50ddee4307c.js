2c4c93ccfd8cd6428ca8f7315f6767a2d74292d4
SeptiyaTamara <septiyakristin98@mail.ugm.ac.id>
2019-04-13 08:29:48 +0700

    Updates

---
+++ precache-manifest.93a693f2a35177f88181b50ddee4307c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0f0cc91f27390308c56",
+    "url": "/Utsweb3/static/js/runtime~main.8c6a43cb.js"
+  },
+  {
+    "revision": "d5912782a5d764621e8d",
+    "url": "/Utsweb3/static/js/main.a3ae26d9.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Utsweb3/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d5912782a5d764621e8d",
+    "url": "/Utsweb3/static/css/main.2900f634.chunk.css"
+  },
+  {
+    "revision": "775aac75570a144bb76c332507996cd8",
+    "url": "/Utsweb3/index.html"
+  }
+];
